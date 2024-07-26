20b55479e2001914e269cdfe3f00c967b46583fb
ikkyuuq <kittipongprasompong@gmail.com>
2024-07-26 15:46:10 +0700

    feat(GameManager): implement enemy spawn

--- Assets/Scenes/Scripts/GameManager.cs
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 public class GameManager : MonoBehaviour{
     public GameObject playerPrefab, enemyPrefab;
-    public float playerMoveSpeed = 5f;
+    public float playerMoveSpeed = 5f, enemySpawnTime = 0f;
     private GameObject player;
-    private List<GameObject> enemies, projectiles = new List<GameObject>();
+    private List<GameObject> enemies = new List<GameObject>(), projectiles = new List<GameObject>();
     void Start(){
         player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
     }
     void Update(){
         PlayerMovement();
+        EnemySpawner();
         //so on... jai dee laew na krub :)
     }
     void PlayerMovement() {
         player.transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * playerMoveSpeed);
+    }
+    void EnemySpawner() {
+        enemySpawnTime += Time.deltaTime;
+        if (enemySpawnTime >= 1f) {
+            Vector3 spawnPosition = player.transform.position + (Vector3)Random.insideUnitCircle.normalized * 10f;
+            enemies.Add(Instantiate(enemyPrefab, spawnPosition, Quaternion.identity));
+            enemySpawnTime = 0;
+        }
     }
 }
