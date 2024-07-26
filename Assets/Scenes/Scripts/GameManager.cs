b8b88ebfbaad13c99f05cf8c0414fc8c17ead53e
ikkyuuq <kittipongprasompong@gmail.com>
2024-07-26 15:02:00 +0700

    feat(GameManager): implement player movement, create player instant every start

--- Assets/Scenes/Scripts/GameManager.cs
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 public class GameManager : MonoBehaviour{
     public GameObject playerPrefab, enemyPrefab;
+    public float playerMoveSpeed = 5f;
     private GameObject player;
     private List<GameObject> enemies, projectiles = new List<GameObject>();
     void Start(){
         player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
     }
     void Update(){
-
+        PlayerMovement();
+        //so on... jai dee laew na krub :)
+    }
+    void PlayerMovement() {
+        player.transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * playerMoveSpeed);
     }
 }
